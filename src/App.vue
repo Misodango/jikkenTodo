19baa6abe3f8fa25265657d0aa1dd6a9bd3aa5f8
Misodango <kazu.kazu.tub@gmail.com>
2024-10-31 22:23:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<!-- <template> -->
+<!--   <img alt="Vue logo" src="./assets/logo.png"> -->
+<!--   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+<!-- </template> -->
+<!-- src/App.vue -->
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>Hello, Vuetify!</v-container>
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </v-main>
+  </v-app>
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
