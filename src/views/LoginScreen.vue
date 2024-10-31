e3661cf33153fc66a42b1f135700f3f452e06b72
Misodango <kazu.kazu.tub@gmail.com>
2024-10-31 23:20:04 +0900

    firebaseAuth設定

--- src/views/LoginScreen.vue
+++ src/views/LoginScreen.vue
@@ -1,12 +1,37 @@
 <template>
-  login
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+    <p>{{ message }}</p>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'LoginScreen',
-  methods: {
+import { auth } from "../firebase/init";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
-  }
-}
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      message: "",
+    };
+  },
+  methods: {
+    async handleLogin() {
+      try {
+        await signInWithEmailAndPassword(auth, this.email, this.password);
+        this.message = "Login successful!";
+        this.$router.push("/dashboard"); // ダッシュボード画面に遷移（例）
+      } catch (error) {
+        this.message = error.message;
+      }
+    },
+  },
+};
 </script>
--- src/views/SignupScreen.vue
+++ src/views/SignupScreen.vue
@@ -1,12 +1,37 @@
 <template>
-  signup
+  <div>
+    <h1>Sign Up</h1>
+    <form @submit.prevent="handleSignup">
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Sign Up</button>
+    </form>
+    <p>{{ message }}</p>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'SignupScreen',
-  methods: {
+import { auth } from "../firebase/init";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-  }
-}
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      message: "",
+    };
+  },
+  methods: {
+    async handleSignup() {
+      try {
+        await createUserWithEmailAndPassword(auth, this.email, this.password);
+        this.message = "Signup successful!";
+        this.$router.push("/login"); // ログイン画面に遷移
+      } catch (error) {
+        this.message = error.message;
+      }
+    },
+  },
+};
 </script>
